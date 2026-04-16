552d93f04deae8b81a06ea977185fb66e3b34ee0
Luan Holanda <luanholanda.dev@gmail.com>
2026-04-16 11:19:17 -0300

    Espressoes booleanas

---
+++ logica-python/05-complementares/ex03.py
@@ -0,0 +1,14 @@
+#Escreva as seguintes expressoes booleanas em linguagem python
+
+#a)O somatorio de 2 com 2 é menor que 4
+a = 2 + 2 < 4
+print(a)
+#b)O valor 7 // 3 é igual 1 + 1
+b = 7 // 3 == 1 + 1
+print(b)
+#c)A soma de 3 elevado ao quadrado com 4 elevado ao quadrado é igual a 25
+c = (3**2 + 4**2) == 25
+print(c)
+#d)A soma de 2, 4 e 6 é maior que 12
+d = (2 + 4 + 6) > 12
+print(d)
