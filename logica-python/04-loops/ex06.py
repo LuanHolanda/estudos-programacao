a4d057cf8ee2beafbc274616d5ecd7f46d355756
Luan Holanda <luanholanda.dev@gmail.com>
2026-04-19 12:04:19 -0300

    tabuada com while e for

--- logica-python/04-loops/ex06.py
+++ logica-python/04-loops/ex06.py
@@ -24,3 +24,27 @@ while (multiplicador <= 10):
 # 3. Quando mostrar o resultado?
 
 # 👉 (print no lugar certo)
+
+# Forma do professor
+# 2x while
+# tabuada = 1
+# while tabuada <= 10:
+#   print(f'TABUADA DO {tabuada}')
+#   i = 1
+#   while i <= 10:
+#     print(f'{tabuada} x {i} = {tabuada * i}')
+#   tabuada += 1
+
+#2x for
+# for tabuada in range (1, 11, 1):
+#   print(f'TABUADA DO {tabuada}:')
+#   for i in range(1, 11, 1):
+#     print(f'{tabuada} x {i} = {tabuada * i} ')
+
+#while + for
+# tabuada = 1
+# while tabuada <= 10:
+#   print(f'TABUADA DO {tabuada}:')
+#   for i in range (1, 11, 1):
+#     print(f'{tabuada} x {i} = {tabuada * i}')
+#   tabuada += 1
